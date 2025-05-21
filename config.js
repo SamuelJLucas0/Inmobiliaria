0bfb7c10e8cb5c23dff5b582a198fe280d8cda21
Samuel_Juarez_Lucas <samueljuarezlucas.90@gmail.com>
2025-05-20 21:36:15 -0600

    prueba

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const PORT = process.env.PORT || 3000;
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_USER = process.env.DB_USER || 'root';
-const DB_PASSWORD = process.env.DB_PASSWORD || 'pass';
-const DB_NAME = process.env.DB_NAME || 'usersdb';
+const DB_PASSWORD = process.env.DB_PASSWORD || '';
+const DB_NAME = process.env.DB_NAME || 'inmobiliaria';
 const DB_PORT = process.env.DB_PORT || '3306';
 
 module.exports = {
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use("/usuarios", usuarios);
-app.use(auth);
+//app.use(auth);
 
 
 app.listen(PORT, () => {
